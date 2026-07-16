9c2dc94bf307fed7938459daf42538de87ed6b95
Md Gulfam <mdgulfam0505@gmail.com>
2026-07-16 12:45:42 +0530

    adding analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AdminDashboard from "./pages/AdminDashboard";
 import Blog from "./pages/Blog";
 import RegisterBrand from "./pages/RegisterBrand";
 import RegisterInfluencer from "./pages/RegisterInfluencer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const path = window.location.pathname;
